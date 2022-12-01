53294d16c1fa624097afef36a548ea7d2391ba65
Justin Ma <justinma@yatzima.local>
2022-12-01 21:39:21 +0100

    Update day_1 solution with new variable names
    Day_1 solution was updated with new variable names.

--- advent-of-code-2022/day_1/solution.py
+++ advent-of-code-2022/day_1/solution.py
@@ -3,24 +3,25 @@ def main():
         data = f.read().splitlines()
         
     elf_cals = []
-    cal = 0
+    sum_cal = 0
     max_cal = 0
 
     for elm in data:
         if elm == '':
             elf_cals.append(cal)
-            if cal > max_cal:
+            if sum_cal > max_cal:
                 max_cal = cal
-            cal = 0
+            sum_cal = 0
         else:
-            cal += int(elm)
+            sum_cal += int(elm)
     
     elf_cals.sort()
     
     solution_a = str(max_cal)
     solution_b = str(sum(elf_cals[-3:]))
-    print('The total calories of the elf who carries the most is: ' + solution_a + 'calories')
-    print('The total calories carried by the top three elfs is: ' + solution_b + 'calories')
+
+    print('The total calories of the elf who carries the most is: ' + solution_a + ' calories')
+    print('The total calories carried by the top three elfs is: ' + solution_b + ' calories')
 
     
 if __name__ == "__main__":
