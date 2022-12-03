bf3d08c034a44479605c2c0285c1f63b4841c770
yatzima <justinmazk@gmail.com>
2022-12-03 14:05:40 +0100

    Update solution structure to be consistent
    The soution script now writes an output.txt file for the answers. This
    is consistent with how I've written for the other days.

--- advent-of-code-2022/day_1/solution.py
+++ advent-of-code-2022/day_1/solution.py
@@ -1,27 +1,36 @@
-def main():
-    with open('data.txt', 'r') as f:
+def load_data():
+    with open('input.txt', 'r') as f:
         data = f.read().splitlines()
-        
+    return data
+    
+
+def write_data(solution1, solution2):
+    with open('output.txt', 'w') as f:
+        f.write(solution1)
+        f.write('\n')
+        f.write(solution2)
+
+
+def main():
+    data = load_data() 
     elf_cals = []
     sum_cal = 0
     max_cal = 0
 
     for elm in data:
         if elm == '':
-            elf_cals.append(cal)
+            elf_cals.append(sum_cal)
             if sum_cal > max_cal:
-                max_cal = cal
+                max_cal = sum_cal
             sum_cal = 0
         else:
             sum_cal += int(elm)
     
     elf_cals.sort()
     
-    solution_a = str(max_cal)
-    solution_b = str(sum(elf_cals[-3:]))
-
-    print('The total calories of the elf who carries the most is: ' + solution_a + ' calories')
-    print('The total calories carried by the top three elfs is: ' + solution_b + ' calories')
+    solution_1 = str(max_cal)
+    solution_2 = str(sum(elf_cals[-3:]))
+    write_data(solution_1, solution_2)
 
     
 if __name__ == "__main__":
